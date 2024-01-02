3fbf393778cc92443fdb85cca496615468ca05dc
QuentinGP23 <quentgeoffpro@yahoo.com>
2024-01-02 20:34:40 +0100

    2eme commit

---
+++ No-security/connexion.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <main class="connexion">
+        <form>
+            <h1>Connexion</h1>
+            <label for="">Email</label>
+            <input type="text" name="" id="">
+            <label for="">Mot de passe</label>
+            <input type="password" name="" id="">
+        </form>
+    </main>
+</body>
+
+</html>
